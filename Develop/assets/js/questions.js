562a378dc70ad2ad78c708d6267ef51b6d6ee0d3
Margot Cooper Gruen <margotgruen@gmail.com>
2022-07-12 17:12:47 -0700

    added questions and answers in JS file

--- Develop/assets/js/questions.js
+++ Develop/assets/js/questions.js
@@ -1,2 +1,47 @@
 // TODO: store all questions here
 // NOTE: hint - can use an array that contains objects
+const questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: [
+      { text: "strings", correct: false },
+      { text: "booleans", correct: false },
+      { text: "alerts", correct: true },
+      { text: "numbers", correct: false },
+    ],
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within ____.",
+    answers: [
+        { text: "quotes", correct: false },
+        { text: "curly brackets", correct: false },
+        { text: "parentheses", correct: true },
+        { text: "square brackets", correct: false },
+    ]
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _____.",
+    answers: [
+        {text: "numbers and strings", correct: false },
+        {text: "other arrays", correct: false },
+        {text: "booleans", correct: false },
+        {text: "all of the above", correct: true },
+    ]
+  },
+  {
+    question: "String values must be enclosed within _____ when when being assigned to variables.",
+    answers: [
+        {text: "commas", correct: false },
+        {text: "curly brackets", correct: false }
+        {text: "quotes", correct: true }
+        {text: "parantheses", correct: false },   
+    ]
+  }
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+        {text: "JavaScript", correct: false },
+        {text: "terminal/bash", correct: false },
+        {text: "for loops", correct: false },
+        {text: "console.log", correct: true },
+    ]
+];
