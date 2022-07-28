effabeac0b3be5b8378a0b8a5923a9bd39909a24
Margot Cooper Gruen <margotgruen@gmail.com>
2022-07-28 13:06:24 -0700

    added JS logic for start button, timer, and displaying questions

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,10 +1,62 @@
 // TODO: add game logic
 //make timer start and first question/card show when start button is clicked
-
+console.log(questions);
+var startBtn = document.getElementById("startBtn");
+var timeEl = document.getElementById("timeleft");
+var timeLeft, currentQuestion;
 //show question and corresponding answers
 
+function startQuiz() {
+  timeLeft = 60;
+  startTimer();
+  startBtn.setAttribute("disabled", "true");
+  currentQuestion = 0;
+  showQuestion();
+}
+//add later - remove main menu section when quiz is started
+
+function startTimer() {
+  var newTimer = setInterval(function () {
+    timeLeft--;
+
+    timeEl.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(newTimer);
+    }
+  }, 1000);
+}
+
+function showQuestion() {
+  document.getElementById("question").textContent =
+    questions[currentQuestion].question;
+
+  document.getElementById("btnOne").textContent =
+    questions[currentQuestion].answers[0].text;
+  document
+    .getElementById("btnOne")
+    .setAttribute(
+      "data-boolean",
+      questions[currentQuestion].answers[0].correct
+    );
+}
 //if correct answer is clicked, display on webpage under answers 'Correct!' and show next question
 
+function checkAnswer(event) {
+  console.log(event.target);
+  var isCorrect = event.target.dataset.boolean;
+
+  if (isCorrect == "true") {
+    console.log("yo das good");
+  } else {
+    console.log("nah das whack");
+  }
+
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    showQuestion();
+  }
+}
 //else, display 'Wrong' under answers and go to next question
 
 //show final page when quiz is over - 'All done!'
@@ -15,3 +67,6 @@
 //add 'Submit' button
 
 //display highscores.html with this attempt's score appended into a sorted list, from largest to smallest.
+
+startBtn.addEventListener("click", startQuiz);
+document.getElementById("btnOne").addEventListener("click", checkAnswer);
--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -13,35 +13,39 @@ const questions = [
   {
     question: "The condition in an if/else statement is enclosed within ____.",
     answers: [
-        { text: "quotes", correct: false },
-        { text: "curly brackets", correct: false },
-        { text: "parentheses", correct: true },
-        { text: "square brackets", correct: false },
-    ]
+      { text: "parentheses", correct: true },
+      { text: "quotes", correct: false },
+      { text: "curly brackets", correct: false },
+      { text: "square brackets", correct: false },
+    ],
   },
   {
     question: "Arrays in JavaScript can be used to store _____.",
     answers: [
-        {text: "numbers and strings", correct: false },
-        {text: "other arrays", correct: false },
-        {text: "booleans", correct: false },
-        {text: "all of the above", correct: true },
-    ]
+      { text: "numbers and strings", correct: false },
+      { text: "other arrays", correct: false },
+      { text: "booleans", correct: false },
+      { text: "all of the above", correct: true },
+    ],
   },
   {
-    question: "String values must be enclosed within _____ when when being assigned to variables.",
+    question:
+      "String values must be enclosed within _____ when when being assigned to variables.",
     answers: [
-        {text: "commas", correct: false },
-        {text: "curly brackets", correct: false }
-        {text: "quotes", correct: true }
-        {text: "parantheses", correct: false },   
-    ]
-  }
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      { text: "commas", correct: false },
+      { text: "curly brackets", correct: false },
+      { text: "quotes", correct: true },
+      { text: "parantheses", correct: false },
+    ],
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
     answers: [
-        {text: "JavaScript", correct: false },
-        {text: "terminal/bash", correct: false },
-        {text: "for loops", correct: false },
-        {text: "console.log", correct: true },
-    ]
+      { text: "JavaScript", correct: false },
+      { text: "terminal/bash", correct: false },
+      { text: "for loops", correct: false },
+      { text: "console.log", correct: true },
+    ],
+  },
 ];
