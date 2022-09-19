add152a659b3dbb2e70fd3a43887ea7b0cb8702c
Margot Cooper Gruen <margotgruen@gmail.com>
2022-09-19 15:54:45 -0700

    Help from Dru during tutoring. Set up function to work to start quiz and list questions on webpage.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -3,16 +3,12 @@
 console.log(questions);
 var startBtn = document.getElementById("startBtn");
 var timeEl = document.getElementById("timeleft");
-var timeLeft, currentQuestion;
-//show question and corresponding answers
+var answerButtons = document.querySelector(".btn-grid");
+var currentQuestion = 0;
+var btnArray = document.querySelectorAll(".btn");
 
-function startQuiz() {
-  timeLeft = 60;
-  startTimer();
-  startBtn.setAttribute("disabled", "true");
-  currentQuestion = 0;
-  showQuestion();
-}
+//show question and corresponding answers
+var timeLeft = 60;
 //add later - remove main menu section when quiz is started
 
 function startTimer() {
@@ -28,35 +24,32 @@ function startTimer() {
 }
 
 function showQuestion() {
-  document.getElementById("question").textContent =
-    questions[currentQuestion].question;
-
-  document.getElementById("btnOne").textContent =
-    questions[currentQuestion].answers[0].text;
-  document
-    .getElementById("btnOne")
-    .setAttribute(
-      "data-boolean",
-      questions[currentQuestion].answers[0].correct
-    );
+  // document.getElementById("btnOne").textContent =
+  //   questions[currentQuestion].answers[0].text;
+  // document
+  //   .getElementById("btnOne")
+  //   .setAttribute(
+  //     "data-boolean",
+  //     questions[currentQuestion].answers[0].correct
+  //   );
 }
 //if correct answer is clicked, display on webpage under answers 'Correct!' and show next question
 
-function checkAnswer(event) {
-  console.log(event.target);
-  var isCorrect = event.target.dataset.boolean;
+// function checkAnswer(event) {
+//   console.log(event.target);
+//   var isCorrect = event.target.dataset.boolean;
 
-  if (isCorrect == "true") {
-    console.log("yo das good");
-  } else {
-    console.log("nah das whack");
-  }
+//   if (isCorrect == "true") {
+//     console.log("yo das good");
+//   } else {
+//     console.log("nah das whack");
+//   }
 
-  currentQuestion++;
-  if (currentQuestion < questions.length) {
-    showQuestion();
-  }
-}
+//   currentQuestion++;
+//   if (currentQuestion < questions.length) {
+//     showQuestion();
+//   }
+// }
 //else, display 'Wrong' under answers and go to next question
 
 //show final page when quiz is over - 'All done!'
@@ -65,8 +58,28 @@ function checkAnswer(event) {
 //add field to 'Enter initials:'
 
 //add 'Submit' button
-
 //display highscores.html with this attempt's score appended into a sorted list, from largest to smallest.
+function startQuiz() {
+  console.log(questions.length);
 
-startBtn.addEventListener("click", startQuiz);
-document.getElementById("btnOne").addEventListener("click", checkAnswer);
+  // while (currentQuestion < question.length) {
+  startTimer();
+  startBtn.setAttribute("disabled", "true");
+  // showQuestion();
+  var questionEl = document.getElementById("questions");
+  console.log(questionEl[currentQuestion]);
+  for (let i = 0; i < btnArray.length; i++) {
+    btnArray[i].innerHTML = questions[currentQuestion].answers[i].text;
+  }
+  questionEl.textContent = questions[currentQuestion].question;
+  // answerButtons.innerHTML = questions[currentQuestion].answers;
+  //textContent and innerHTML do the same thing practically
+}
+// }
+console.log(questions);
+startBtn.addEventListener("click", (event) => {
+  console.log(event);
+  startQuiz();
+});
+
+// document.getElementById("btnOne").addEventListener("click", checkAnswer);
